4b03f7729ebff7880aa5538acdf0da250a04bb93
KwesiSai <kwesiabraham4503@gmail.com>
2023-08-31 15:43:17 +0000

    getting started in Javascript

--- gettingstarted.js
+++ gettingstarted.js
@@ -1,1 +1,6 @@
-document.getElementById('demo').innerHTML="Hello Javascript";
+console.log("HELLO, this is my first JS file");
+prompt("Are you excited? ")
+alert("Pardon me i wanna use these cos i can, t4thanks!")
+let total=1+3;
+ console.log("Adios Supremos")
+ 
