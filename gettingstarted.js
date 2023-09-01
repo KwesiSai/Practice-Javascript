c6354f9b93cef081196ce05245ec53718c241963
KwesiSai <kwesiabraham4503@gmail.com>
2023-09-01 14:54:59 +0000

    Understanding decisions and logical operators

--- gettingstarted.js
+++ gettingstarted.js
@@ -1,10 +1,18 @@
-let answer = prompt('Having fun?').toLowerCase();
-if(answer==="yes",'yeah','yep','yah'){
-    console.log("Wonderful wonderful, i am glad you are havivng fun" );
-}
-else if(answer==="no"){
-    console.warn("You are not loved at home!!!!")
+// let answer = prompt('Having fun?').toLowerCase();
+// if(answer==="yes",'yeah','yep','yah'){
+//     console.log("Wonderful wonderful, i am glad you are havivng fun" );
+// }
+// else if(answer==="no"){
+//     console.warn("You are not loved at home!!!!")
+// }
+// else{
+//     console.log("hmm, enjoy the rest of your day");
+// }
+
+let password= prompt("Enter a password");
+if(password.length>=6 && password.indexOf(' ')===-1){
+    console.log("Valid Password");
 }
 else{
-    console.log("hmm, enjoy the rest of your day");
+    console.error("Password is either too short or has a space or both!")
 }
